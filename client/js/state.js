c7aa8a34fe47f2ff9ce32f0203b90821fac4848c
protosamm <satyamkachhi000@gmail.com>
2026-04-21 18:07:51 +0530

    adding multiplayer

---
+++ client/js/state.js
@@ -0,0 +1,23 @@
+export const state = {
+  id: null,
+  tool: 'pen',       // active tool
+  strokeColor: '#1a1614',  // active stroke color
+  strokeWidth: 5,    // active stroke width
+  fill: false,
+  fillColor: '#1a1614',  // active fill color
+  clipColors: false,
+  
+  // GLOBAL FLAGS
+  isErasing: false,
+  isSpaceDown: false,
+
+  strokes: [],
+  undoStack: [],
+  redoStack: [],
+  
+  currentStroke: null, // the stroke being drawn right now
+  currentShape: null,
+
+  mouseX: 0,   //Mouse position
+  mouseY: 0
+};
---
+++ client/js/tools/line.js
@@ -0,0 +1,50 @@
+import { state } from "../state.js";
+import { camera, screenToWorld } from "../camera.js";
+import { generateId } from '../utils.js';
+
+export function lineDown(e, canvas) {
+    const pos = screenToWorld(e.clientX, e.clientY);
+
+    state.currentShape = {
+        id: generateId(),
+        type: 'line', 
+        startX: pos.x,
+        startY: pos.y,
+        endX: pos.x,
+        endY: pos.y,
+        strokeColor: state.strokeColor,
+        strokeWidth: state.strokeWidth / camera.zoom
+    }
+}
+
+export function lineMove(e, canvas) {
+    const pos = screenToWorld(e.clientX, e.clientY);
+
+    if(state.currentShape) {
+        state.currentShape.endX = pos.x;
+        state.currentShape.endY = pos.y;
+    };
+}
+
+export function lineUp() {
+    if(state.currentShape) {
+        const line = state.currentShape;
+        state.strokes.push(line);
+        
+        state.undoStack.push({
+            undo: () => state.strokes.splice(state.strokes.indexOf(line), 1),
+            redo: () => state.strokes.push(line)
+        })
+        state.redoStack = [];
+        state.currentShape = null;
+    }
+}
+
+export function drawLine(ctx, line) {
+    ctx.beginPath();
+    ctx.moveTo(line.startX, line.startY);
+    ctx.lineTo(line.endX, line.endY);
+    ctx.lineWidth = line.strokeWidth
+    ctx.strokeStyle = line.strokeColor;
+    ctx.stroke();
+}
